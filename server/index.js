22b21dd05e83b1f94fca39419b2ccabed6b69c40
Bryce Champaign <bechampaign@protonmail.com>
2020-02-21 17:02:11 -0500

    Configure router middleware

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const { PORT } = process.env; // access PORT environment variable (check docker-compose.yml to change)
+const router = require('./routes');
 
 // Serving static files
 app.use(express.static(path.join(__dirname, '../dist')));
+
+// Configure router as middleware for handling all endpoints
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
