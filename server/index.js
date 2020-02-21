386e8d7afe05ad722b5bbfb79908b01014c7a654
Bryce Champaign <bechampaign@protonmail.com>
2020-02-21 18:07:23 -0500

    Implement body-parser middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const { PORT } = process.env; // access PORT environment variable (check docker-compose.yml to change)
+const bodyParser = require('body-parser');
 const router = require('./routes');
+const app = express();
+const { PORT } = process.env; // Access PORT environment variable (check docker-compose.yml to change)
 
 // Serving static files
 app.use(express.static(path.join(__dirname, '../dist')));
+app.use(bodyParser.json());
 
 // Configure router as middleware for handling all endpoints
 app.use('/', router);
