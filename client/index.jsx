30b2a458181823ba38621719bb00f8ddda6afeb4
Bryce Champaign <bechampaign@protonmail.com>
2020-02-21 10:47:35 -0500

    Create placeholder frontend
    A basic App component which renders placeholder text has been created and is rendered.

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return <h1>Placeholder</h1>;
+};
+
+render(<App />, document.getElementById('root'));
