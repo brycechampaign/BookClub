987fa367e6f29f41e25a57be8b1765f4aa476d12
Bryce Champaign <bechampaign@protonmail.com>
2020-02-21 14:20:51 -0500

    Add comments to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,15 @@
+/* Remove tables if they already exist so the schema can be re-written */
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS books CASCADE;
 
+/* Create a user table, schema subject to change as genuine authentication is implemented */
 CREATE TABLE users(
   id serial PRIMARY KEY,
   name VARCHAR(255),
   password VARCHAR(255)
 );
 
+/* Create books table for storing book metadata */
 CREATE TABLE books(
   id serial PRIMARY KEY,
   user_id INT REFERENCES users(id),
@@ -16,6 +19,7 @@ CREATE TABLE books(
   description VARCHAR(255)
 );
 
+/* Create table to store notes associated with books */
 CREATE TABLE notes(
   id serial PRIMARY KEY,
   book_id INT REFERENCES books(id),
