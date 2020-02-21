90fefc002f860f4ec33e49830f7a0ed8e204fd6b
Bryce Champaign <bechampaign@protonmail.com>
2020-02-21 12:59:55 -0500

    Update server PORT value
    The PORT variable is now set to the value of the PORT environment variable, which is
    configured in the docker-compose.yml file.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env; // access PORT environment variable (check docker-compose.yml to change)
 
 // Serving static files
 app.use(express.static(path.join(__dirname, '../dist')));
