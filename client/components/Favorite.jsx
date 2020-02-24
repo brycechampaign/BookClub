efbdd203b17fbaa000554838d051c927d73e9e33
Bryce Champaign <bechampaign@protonmail.com>
2020-02-23 19:41:08 -0500

    Implement button for deleting a book
    Each `Favorite` component now has a delete button for deleting that favorite from the books
    database and refreshing the list.

--- client/components/Favorite.jsx
+++ client/components/Favorite.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { deleteBook } from '../api';
 
-const Favorite = ({ title, author, genre, description }) => {
+const Favorite = ({
+  title,
+  author,
+  genre,
+  description,
+  updateFavorites,
+  id
+}) => {
+  const deleteFavorite = () => {
+    deleteBook(id).then(() => updateFavorites());
+  };
+
   return (
     <div className="favorite card">
       <h3>{title}</h3>
       <h4>{description}</h4>
       <h5>{author}</h5>
       <h5>{genre}</h5>
+      <button onClick={deleteFavorite}>Delete</button>
     </div>
   );
 };
