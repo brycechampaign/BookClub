3f78b547890515c44bbb26126e7299c360ebb8f5
Bryce Champaign <bechampaign@protonmail.com>
2020-02-23 23:51:13 -0500

    Implement toggle functionality
    The toggle button now changes text based on if the Note List is displayed, and will hide/show
    the note list depending on if the note list is displayed or not.

--- client/components/NoteToggle.jsx
+++ client/components/NoteToggle.jsx
@@ -5,7 +5,9 @@ const NoteToggle = ({ bookId }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
     <>
-      <button onClick={() => setIsExpanded(true)}>Show Notes</button>
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? 'Hide Notes' : 'Show Notes'}
+      </button>
       {isExpanded ? <NoteList bookId={bookId} /> : null}
     </>
   );
