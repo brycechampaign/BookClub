aadcd2d1e3d227992a98de141b2a55afeb66f46e
Bryce Champaign <bechampaign@protonmail.com>
2020-02-26 00:34:42 -0500

    Implement Signup component
    The Signup component contains a form for creating a new account. After a client submits new
    credentials (creates a username and password) they are authenticated as that new user and
    redirected to the overview page.

---
+++ client/components/Signup.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Auth from '../auth';
+import { createUser } from '../api';
+import { Redirect } from 'react-router-dom';
+
+const Signup = () => {
+  const [redirectToHome, setRedirectToHome] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = (username, password) => {
+    Auth.authenticate(username, password, () => {
+      setRedirectToHome(true);
+    });
+  };
+
+  const handleSignup = e => {
+    e.preventDefault();
+
+    // Create a new user, log them in, and redirect to Home page
+    createUser(username, password).then(() => login(username, password));
+  };
+
+  if (redirectToHome === true) {
+    return <Redirect to={`/${username}`} />;
+  }
+
+  return (
+    <div className="auth-container">
+      <form className="card auth-form" onSubmit={e => handleSignup(e)}>
+        <h1>Create an Account</h1>
+
+        <div className="label-input-pair">
+          <label htmlFor="username">Username</label>
+          <input
+            name="username"
+            id="username"
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+
+        <div className="label-input-pair">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            id="password"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <input type="submit" value="Sign Up" />
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
