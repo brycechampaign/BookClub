93593522688109cc6e4d3f0398b2aa9a44ccfe66
Bryce Champaign <bechampaign@protonmail.com>
2020-02-26 00:30:31 -0500

    Remove card class from FavoriteList component

--- client/components/FavoriteList.jsx
+++ client/components/FavoriteList.jsx
@@ -5,7 +5,7 @@ import AddBookForm from './AddBookForm';
 // Displays a list of the current user's favorite books
 const FavoriteList = ({ favorites, updateFavorites, user }) => {
   return (
-    <div id="favorite-list" className="card">
+    <div id="favorite-list">
       <h2>Favorite Books</h2>
       <div id="favorites-container">
         {favorites.map(book => {
